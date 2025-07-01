af5b407f1499995ed904fd67af463654c77ded3e
avishaigal <avishai.gal@gmail.com>
2025-07-01 13:31:33 +0300

    Change ball onclick function

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ var gBallSize = 100
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
-    gBallSize += 50
+    
+    gBallSize >= 400 ? gBallSize = 100 : gBallSize += 50
 
     elBall.style.height = gBallSize
     elBall.style.width = gBallSize
-    elBall.innerText = elBall.style.height
+    elBall.innerText = gBallSize
 }
