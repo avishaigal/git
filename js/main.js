3e5296ea7b3c51204ed877caf7cde078ac212f66
avishaigal <avishai.gal@gmail.com>
2025-07-01 18:37:03 +0300

    Add ball4 reduce size function

--- js/main.js
+++ js/main.js
@@ -16,14 +16,28 @@ function onBallClick(elBall) {
 }
 
 function onSwitchAttributes() {
-const elBall1 = document.querySelector('.ball1')
-const elBall2 = document.querySelector('.ball2')
-const tempBallBackgroundColorHolder = elBall1.style.backgroundColor
-const tempBallSizeHolder = elBall1.style.height
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
+    const tempBallBackgroundColorHolder = elBall1.style.backgroundColor
+    const tempBallSizeHolder = elBall1.style.height
 
-elBall1.style.backgroundColor = elBall2.style.backgroundColor
-elBall1.style.height = elBall2.style.height
+    elBall1.style.backgroundColor = elBall2.style.backgroundColor
+    elBall1.style.height = elBall2.style.height
 
-elBall2.style.backgroundColor = tempBallBackgroundColorHolder
-elBall2.style.height = tempBallSizeHolder
+    elBall2.style.backgroundColor = tempBallBackgroundColorHolder
+    elBall2.style.height = tempBallSizeHolder
+}
+
+function onReduceBallsSize() {
+    
+    for (var i = 1; i <= 2; i++) {
+        const elBall = document.querySelector(`.ball${i}`)
+        const randSize = getRandomInt(20, 60)
+        const minDiameter = gBallSize - randSize
+        
+        minDiameter <= 100 ? gBallSize = 100 : gBallSize -= randSize
+        elBall.style.height = gBallSize
+        elBall.style.width = gBallSize
+        elBall.innerText = gBallSize
+    }
 }
