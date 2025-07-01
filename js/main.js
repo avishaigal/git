a23b9340f5b4ba0f9c0046402ba30cb1edfb6d8e
avishaigal <avishai.gal@gmail.com>
2025-07-01 14:35:53 +0300

    Add ball random color onclick

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ var gBallSize = 100
 function onBallClick() {
     const elBall = document.querySelector('.ball')
     const randSize = getRandomInt(20, 60)
+    const elBallColor = getRandomColor()
 
     gBallSize + randSize >= 400 ? gBallSize = 100 : gBallSize += randSize
 
     elBall.style.height = gBallSize
     elBall.style.width = gBallSize
     elBall.innerText = gBallSize
+    elBall.style.backgroundColor = elBallColor
 }
--- js/util.js
+++ js/util.js
@@ -6,3 +6,11 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
