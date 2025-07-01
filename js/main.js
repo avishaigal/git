8127ac5bf0f87e98f3fca4662416dd9eb800a113
avishaigal <avishai.gal@gmail.com>
2025-07-01 13:28:27 +0300

    Revert "Change ball style js"
    This reverts commit bb77e22c184aa90bec685af8f52d79243f24dd04.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,9 @@ var gBallSize = 100
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
-    
-    gBallSize >= 400 ? gBallSize = 100 : gBallSize += 50
+    gBallSize += 50
 
     elBall.style.height = gBallSize
     elBall.style.width = gBallSize
-    elBall.innerText = gBallSize
+    elBall.innerText = elBall.style.height
 }
