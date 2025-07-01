3f086aa225155eb3deb32689ed8ad3a6e0057377
avishaigal <avishai.gal@gmail.com>
2025-07-01 14:33:12 +0300

    Add ball random grow size

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ var gBallSize = 100
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
-    
-    gBallSize >= 400 ? gBallSize = 100 : gBallSize += 50
- 
+    const randSize = getRandomInt(20, 60)
+
+    gBallSize + randSize >= 400 ? gBallSize = 100 : gBallSize += randSize
+
     elBall.style.height = gBallSize
     elBall.style.width = gBallSize
     elBall.innerText = gBallSize
